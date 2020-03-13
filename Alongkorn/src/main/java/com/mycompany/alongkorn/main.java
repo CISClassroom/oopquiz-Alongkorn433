cd30e7fc94228d9a411093954b8b81de6e63e450
อลงกรณ์ ประดิษฐวงษ์ <59078983+Alongkorn433@users.noreply.github.com>
2020-03-13 15:57:02 +0700

    623410037-1

---
+++ Alongkorn/src/main/java/com/mycompany/alongkorn/main.java
@@ -0,0 +1,415 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.alongkorn;
+
+/**
+ *
+ * @author acer
+ */
+public class main {
+    public void  main(String[] args) {
+
+        studen make = new studen();
+
+       make.name= "Alongkorn praditthawong";
+        make.address= "Home";
+        make.major= "CIS";
+        make.School="khonkhanuniversity";
+        make.email="drako.7896@hotmail.com";
+        make.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ make.name +" "+ make.major+" "+make.address+" "+make.School+" "+make.email+" "+make.phonnumber);
+
+        studen[] oopStudents = {make,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        
+    }
+public void  main2(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+}
+        public void  main3(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main4(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main5(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main6(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main7(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main8(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main9(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main10(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main11(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main12(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main13(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main14(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main15(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main16(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main17(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main18(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main19(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main20(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+public void  main21(String[] args) {
+
+        studen cat = new studen();
+
+       cat.name= "Alongkorn praditthawong";
+        cat.address= "Home";
+        cat.major= "CIS";
+        cat.School="khonkhanuniversity";
+        cat.email="drako.7896@hotmail.com";
+        cat.phonnumber="0937631801";
+        
+
+        System.out.println("1."+ cat.name +" "+ cat.major+" "+cat.address+" "+cat.School+" "+cat.email+" "+cat.phonnumber);
+
+        studen[] oopStudents = {cat,};
+
+        System.out.println("1."+oopStudents[0].name);
+
+        }
+}
+    
+    
+
